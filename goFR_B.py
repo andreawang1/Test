94095714d9eca6cfb5d8bf584ae20e570ac487ef
unknown <weareets@gmail.com>
2017-08-03 16:33:05 +0800

    test commit

--- goFR_B.py
+++ goFR_B.py
@@ -14,9 +14,10 @@ class goFR():
 
         # What to do you ctrl + c
         rospy.on_shutdown(self.shutdown)
-
+        rospy.init_node("kobuki_button")
+        rospy.Subscriber("/mobile_base/events/button",ButtonEvent,self.ButtonEventCallback)
         self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
-
+        rospy.spin();
 	# 5 HZ
         r = rospy.Rate(5);
 
@@ -31,6 +32,9 @@ class goFR():
         turn_cmd = Twist()
         turn_cmd.linear.x = 0
         turn_cmd.angular.z = radians(45); #45 deg/s in radians/s
+    #stop
+        stop_cmd = Twist()
+        stop_cmd.linear.x = 0
 
         #two keep drawing squares.  Go forward for 2 seconds (10 x 5 HZ) then turn for 2 second
         if ( data.button == ButtonEvent.Button0 ) :
@@ -45,14 +49,30 @@ class goFR():
                 for x in range(0,10) :
                     self.cmd_vel.publish(turn_cmd)
                     r.sleep()
+                for x in range(0,10) :
+                    self.cmd_vel.publish(stop_cmd)
+                    r.sleep()
 
                 rospy.loginfo("finish")
 
-    def shutdown(self):
+#    def shutdown(self):
         # stop turtlebot
-        rospy.loginfo("Stop Drawing Squares")
-        self.cmd_vel.publish(Twist())
-        rospy.sleep(1)
+#        rospy.loginfo("Stop Drawing Squares")
+#        self.cmd_vel.publish(Twist())
+#        rospy.sleep(1)
+    def ButtonEventCallback(self,data):
+	    if ( data.state == ButtonEvent.RELEASED ) :
+		state = "released"
+	    else:
+		state = "pressed"
+	    if ( data.button == ButtonEvent.Button0 ) :
+		button = "B0"
+	    elif ( data.button == ButtonEvent.Button1 ) :
+		button = "B1"
+	    else:
+		button = "B2"
+	    rospy.loginfo("Button %s was %s."%(button, state))
+
 
 if __name__ == '__main__':
     try:
