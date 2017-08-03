87214c249578748a9190363a3395d6204eae9e47
unknown <weareets@gmail.com>
2017-08-03 14:52:31 +0800

    test commit

--- goFR.py
+++ goFR.py
@@ -1,67 +1,67 @@
 #!/usr/bin/env python
 
-'''
-Copyright (c) 2015, Mark Silliman
-All rights reserved.
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-'''
-
-# A very basic TurtleBot script that moves TurtleBot forward indefinitely. Press CTRL + C to stop.  To run:
-# On TurtleBot:
-# roslaunch turtlebot_bringup minimal.launch
-# On work station:
-# python goforward.py
+# An example of TurtleBot 2 drawing a 0.4 meter square.
+# Written for indigo
+#利用draw_a_square 範例改成直走右轉停
 
 import rospy
 from geometry_msgs.msg import Twist
+from math import radians
+count = 0
 
-class GoForward():
+class goFR():
     def __init__(self):
         # initiliaze
-        rospy.init_node('GoForward', anonymous=False)
+        rospy.init_node('drawasquare', anonymous=False)
 
-	# tell user how to stop TurtleBot
-	rospy.loginfo("To stop TurtleBot CTRL + C")
-
-        # What function to call when you ctrl + c
+        # What to do you ctrl + c
         rospy.on_shutdown(self.shutdown)
 
-	# Create a publisher which can "talk" to TurtleBot and tell it to move
-        # Tip: You may need to change cmd_vel_mux/input/navi to /cmd_vel if you're not using TurtleBot2
         self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
 
-	#TurtleBot will stop if we don't keep telling it to move.  How often should we tell it to move? 10 HZ
-        r = rospy.Rate(10);
+	# 5 HZ
+        r = rospy.Rate(5);
 
-        # Twist is a datatype for velocity
+	# create two different Twist() variables.  One for moving forward.  One for turning 45 degrees.
+
+        # let's go forward at 0.2 m/s
         move_cmd = Twist()
-	# let's go forward at 0.2 m/s
         move_cmd.linear.x = 0.2
-	# let's turn at 0 radians/s
-	move_cmd.angular.z = 0
+	# by default angular.z is 0 so setting this isn't required
 
-	# as long as you haven't ctrl + c keeping doing...
-        while not rospy.is_shutdown():
-	    # publish the velocity
-            self.cmd_vel.publish(move_cmd)
-	    # wait for 0.1 seconds (10 HZ) and publish again
-            r.sleep()
+        #let's turn at 45 deg/s
+        turn_cmd = Twist()
+        turn_cmd.linear.x = 0
+        turn_cmd.angular.z = radians(45); #45 deg/s in radians/s
 
+        stop_cmd = Twist()
+        stop_cmd.linear.x = 0
+
+	#two keep drawing squares.  Go forward for 2 seconds (10 x 5 HZ) then turn for 2 second
+
+        while not rospy.is_shutdown()&count<1:
+	    # go forward 0.4 m (2 seconds * 0.2 m / seconds)
+	    rospy.loginfo("Going Straight")
+            for x in range(0,10) :
+                self.cmd_vel.publish(move_cmd)
+                r.sleep()
+	    # turn 90 degrees
+	    rospy.loginfo("Turning")
+            for x in range(0,10) :
+                self.cmd_vel.publish(turn_cmd)
+                count=count+1;
+                r.sleep()
+
+                rospy.loginfo("finish")
 
     def shutdown(self):
         # stop turtlebot
-        rospy.loginfo("Stop TurtleBot")
-	# a default Twist has linear.x of 0 and angular.z of 0.  So it'll stop TurtleBot
-        self.cmd_vel.publish(Twist())
-	# sleep just makes sure TurtleBot receives the stop command prior to shutting down the script
+        rospy.loginfo("Stop Drawing Squares")
+        self.cmd_vel.publish(stop_cmd)
         rospy.sleep(1)
 
 if __name__ == '__main__':
     try:
-        GoForward()
+        goFR()
     except:
-        rospy.loginfo("GoForward node terminated.")
+        rospy.loginfo("node terminated.")
