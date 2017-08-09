d49f4b45584b7bef8011deeeaa33e41af3c518c4
unknown <weareets@gmail.com>
2017-08-09 16:38:59 +0800

    test commit

--- goforward.py
+++ goforward.py
@@ -27,13 +27,13 @@ class GoForward():
 	# tell user how to stop TurtleBot
 	rospy.loginfo("To stop TurtleBot CTRL + C")
 
-        # What function to call when you ctrl + c    
+        # What function to call when you ctrl + c
         rospy.on_shutdown(self.shutdown)
-        
+
 	# Create a publisher which can "talk" to TurtleBot and tell it to move
         # Tip: You may need to change cmd_vel_mux/input/navi to /cmd_vel if you're not using TurtleBot2
         self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
-     
+        rospy.Subscriber("/mobile_base/events/button",ButtonEvent,self.ButtonEventCallback)
 	#TurtleBot will stop if we don't keep telling it to move.  How often should we tell it to move? 10 HZ
         r = rospy.Rate(10);
 
@@ -50,8 +50,8 @@ class GoForward():
             self.cmd_vel.publish(move_cmd)
 	    # wait for 0.1 seconds (10 HZ) and publish again
             r.sleep()
-                        
-        
+
+
     def shutdown(self):
         # stop turtlebot
         rospy.loginfo("Stop TurtleBot")
@@ -59,7 +59,7 @@ class GoForward():
         self.cmd_vel.publish(Twist())
 	# sleep just makes sure TurtleBot receives the stop command prior to shutting down the script
         rospy.sleep(1)
- 
+
 if __name__ == '__main__':
     try:
         GoForward()
