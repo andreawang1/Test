7c99455d62e2e0e19c1fbffff61877067642beca
unknown <weareets@gmail.com>
2017-08-09 16:47:43 +0800

    test commit

--- goFR_B.py
+++ goFR_B.py
@@ -42,7 +42,7 @@ class goFR_B():
 
         #two keep drawing squares.  Go forward for 2 seconds (10 x 5 HZ) then turn for 2 second
 
-        if ( data.button == ButtonEvent.Button0) :
+#        if ( data.button == ButtonEvent.Button0) :
             # go forward 0.4 m (2 seconds * 0.2 m / seconds)
             rospy.loginfo("Going Straight")
             for x in range(0,10) :
@@ -54,8 +54,10 @@ class goFR_B():
                 self.cmd_vel.publish(turn_cmd)
                 r.sleep()
                 rospy.loginfo("finish")
-        else:
-            self.cmd_vel.publish(stop_cmd)
+#        else:
+            for x in range(0,10) :
+                self.cmd_vel.publish(stop_cmd)
+                r.sleep()
 
     def ButtonEventCallback(self,data):
         if ( data.state == ButtonEvent.RELEASED ) :
